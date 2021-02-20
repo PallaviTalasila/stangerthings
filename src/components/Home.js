7276a85caee3d61c97d9002a6e174bd47a4e9222
Jkalna22 <joshuakalna@yhaoo.com>
2021-02-20 12:57:04 -0500

    Merge to main

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Home = () => {
+
+const Home = ({username}) => {
   return (
     <div>
       <div className="content">
--- src/index.js
+++ src/index.js
@@ -1,21 +1,69 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { PostForm, Header, Login, Register, Home, ViewPosts } from "./components";
+import {
+  PostForm,
+  Header,
+  Login,
+  Register,
+  Home,
+  ViewPosts,
+} from "./components";
+import Profile from "./components/Profile";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userToken, setUserToken] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  
   return (
     <div className="app">
       <Router>
-        <Header />
+        <Header
+          username={username}
+          setUsername={setUsername}
+          setPassword={setPassword}
+          loggedIn={loggedIn}
+          setLoggedIn={setLoggedIn}
+        />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login
+                {...props}
+                username={username}
+                setUsername={setUsername}
+                password={password}
+                setPassword={setPassword}
+                userToken={userToken}
+                setUserToken={setUserToken}
+                setLoggedIn={setLoggedIn}
+              />
+            )}
+          />
+          <Route
+            path="/register"
+            render={(props) => (
+              <Register
+                {...props}
+                username={username}
+                setUsername={setUsername}
+                password={password}
+                setPassword={setPassword}
+                userToken={userToken}
+                setUserToken={setUserToken}
+                setLoggedIn={setLoggedIn}
+              />
+            )}
+          />
+          <Route
+            path="/profile"
+            render={(props) => <Profile {...props} username={username} />}
+          />
           <Route path="/posts" component={ViewPosts} />
-          <Route path="/" component= {Home}/>          
-          
+          <Route path="/" component={Home} />
         </Switch>
       </Router>
     </div>
